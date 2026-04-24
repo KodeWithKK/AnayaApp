e4e5f7b845aef50b54d15a49cfbd5dd0b9be731d
KodeWithKK <kodewithkk@gmail.com>
2026-04-24 22:02:49 +0530

    refactor: update esbuild configuration to include nestjs dependencies and build optimization settings

--- apps/api/esbuild.config.js
+++ apps/api/esbuild.config.js
@@ -1,8 +1,28 @@
-require("esbuild").build({
-  entryPoints: ["src/lambda.ts"],
-  outfile: "dist/index.js",
-  bundle: true,
-  platform: "node",
-  target: "node20",
-  external: ["aws-sdk"],
-});
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["src/lambda.ts"],
+    outfile: "dist/index.js",
+    bundle: true,
+    platform: "node",
+    target: "node20",
+    format: "cjs",
+    sourcemap: false,
+    minify: true,
+
+    external: [
+      "aws-sdk",
+
+      "@nestjs/websockets",
+      "@nestjs/websockets/socket-module",
+
+      "@nestjs/microservices",
+      "@nestjs/microservices/microservices-module",
+
+      "@nestjs/graphql",
+
+      "class-transformer/storage",
+    ],
+  })
+  .catch(() => process.exit(1));
